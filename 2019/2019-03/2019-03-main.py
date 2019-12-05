574fd38bd06ebe80966f47f084092f53e08f169f
Michal Dmytruszynski <micdmy2@gmail.com>
2019-12-05 20:58:32 +0100

    2019-03-part-1 solved.

--- 2019/2019-03/2019-03-main.py
+++ 2019/2019-03/2019-03-main.py
@@ -7,24 +7,22 @@ def read_input():
     return
 
 (cable, wire) = read_input()
-print(str(cable))
-print(str(wire))
 
 def get_dir_and_len(direction_str):
    return (direction_str[0], int(direction_str[1:])) 
 
-def get_lengths(cord):
-    L = len(cord)
-    even = L % 2 == 0
-    if even:
-        return (L/2, L/2)
-    else:
-       if (cord[0][0] in "UD"):
-           return (math.floor(L/2) + 1, math.floor(L/2))
-       else:
-           return (math.floor(L/2), math.floor(L/2) +1)
+# def get_lengths(cord):
+#     L = len(cord)
+#     even = L % 2 == 0
+#     if even:
+#         return (L/2, L/2)
+#     else:
+#        if (cord[0][0] in "UD"):
+#            return (math.floor(L/2) + 1, math.floor(L/2))
+#        else:
+#            return (math.floor(L/2), math.floor(L/2) +1)
     
-def get_segments(cord, v_len, h_len):
+def get_segments(cord):
     verts = []
     horiz = []
     v = 0
@@ -34,10 +32,10 @@ def get_segments(cord, v_len, h_len):
         if direction == "U":
             verts.append((h, (v, v + lenght)))
             v += lenght
-        elif direction = "D":
+        elif direction == "D":
             verts.append((h, (v - lenght, v)))
             v -= lenght
-        elif direction = "R":
+        elif direction == "R":
             horiz.append(((h, h + lenght), v))
             h += lenght
         else:
@@ -45,14 +43,21 @@ def get_segments(cord, v_len, h_len):
             h -= lenght
     return (verts, horiz)
 
-
+def find_closest_crossing(v_cords, h_cords):
+    min_dist = float("inf")
+    for vc in v_cords:
+        for hc in h_cords:
+            if hc[0][0] <= vc[0] <= hc[0][1]:
+                if vc[1][0] <= hc[1] <= vc[1][1]:
+                    dist = abs(vc[0]) + abs(hc[1])
+                    if min_dist > dist:
+                        min_dist = dist
+    return min_dist
         
+(v_cables_seg, h_cables_seg) = get_segments(cable)
+(v_wires_seg, h_wires_seg) = get_segments(wire)
 
-v_cables = []
-h_cables = []
-v_wires = []
-h_wires = []
-v_cables_len, h_cables_len = get_lengths(cable)
-v_wires_len, h_wires_len = get_lengths(wire)
-        
+min1 = find_closest_crossing(v_cables_seg, h_wires_seg)
+min2 = find_closest_crossing(v_wires_seg, h_cables_seg)
 
+print("Closest intersetcion to central port has manhattan distance of %d to the central port."%min([min1, min2]))
