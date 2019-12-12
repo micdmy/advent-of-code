3f4d20ca782fc489ab58b41d8d55af0c70a2b9d8
Michal Dmytruszynski <micdmy2@gmail.com>
2019-12-12 19:37:54 +0100

    2019-06-part-2 solved.

--- 2019/2019-06/2019-06-main.py
+++ 2019/2019-06/2019-06-main.py
@@ -29,10 +29,24 @@ for center, sattelite in raw_map:
 
 gw =GameWindow()
 gw.clear()
-graph = nx.DiGraph(orbit_dict)
+graph = nx.Graph(orbit_dict)
 lengths_to_nodes = nx.single_source_shortest_path_length(graph, "COM")
 total_length = sum([lengths_to_nodes[key] for key in lengths_to_nodes])
 print("Total number of orbits is %d"%total_length)
+
+def find_in_dict(searched_val, dictionary):
+    for key, val in dictionary.items():
+        if searched_val in val:
+            return key 
+    return None
+
+#  part2:
+YOU_orbits = find_in_dict("YOU", orbit_dict)
+SAN_orbits = find_in_dict("SAN", orbit_dict)
+
+dist_to_santa = nx.dijkstra_path_length(graph, YOU_orbits, SAN_orbits)
+print("Number of orbits to santa is %d"%dist_to_santa)
+
 pos = nx.random_layout(graph)
 nx.draw_networkx_nodes(graph, pos)
 nx.draw_networkx_edges(graph, pos = pos)
