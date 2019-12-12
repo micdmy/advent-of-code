35c755235a961d1207b54f35fc73ab6b2bbc830f
Michal Dmytruszynski <micdmy2@gmail.com>
2019-12-12 23:15:34 +0100

    2019-12-part-2 solved.

--- 2019/2019-12/2019-12-main.py
+++ 2019/2019-12/2019-12-main.py
@@ -1,8 +1,12 @@
+from math import gcd
+import numpy as np
 
 class Moon():
     def __init__(self, x, y, z, name):
         self.cords = [x, y, z]
         self.vels = [0, 0, 0]
+        self.orig_cords = [x, y, z]
+        self.orig_vels = [0, 0, 0]
         self._name = name
     
     def apply_velocity(self):
@@ -25,6 +29,10 @@ class Moon():
     def get_kinetic_energy(self):
         a = sum([abs(v) for v in self.vels])
         return a
+    
+    def is_in_original_state(self):
+        return self.cords == self.orig_cords and self.vels == self.orig_vels
+
 
 def read_input():
     moons = []
@@ -37,8 +45,47 @@ def read_input():
             moons.append(Moon(*cords, next(name)))
     return moons
 
+def grav(cs, vs, i, j):
+    if cs[i] > cs[j]:
+        vs[i] -= 1
+        vs[j] += 1
+    elif cs[i] < cs[j]:
+        vs[i] += 1
+        vs[j] -= 1
+      
+
+
 moons = read_input()
-for t in range(0, 1000):
+
+def repeats_for_one_coordinates(cords, look_for):
+    xs_c = cords
+    xs_v = [0,0,0,0]
+    counter = 0
+    while True:
+        counter += 1
+        grav(xs_c, xs_v, 0, 1)
+        grav(xs_c, xs_v, 0, 2)
+        grav(xs_c, xs_v, 0, 3)
+        grav(xs_c, xs_v, 1, 3)
+        grav(xs_c, xs_v, 1, 2)
+        grav(xs_c, xs_v, 3, 2)
+        xs_c = [c+v for c, v in zip(xs_c, xs_v)]
+        if xs_v == [0,0,0,0] and xs_c == look_for:
+            break
+    return counter
+
+rx = repeats_for_one_coordinates([m.cords[0] for m in moons], [m.cords[0] for m in moons])
+ry = repeats_for_one_coordinates([m.cords[1] for m in moons], [m.cords[1] for m in moons])
+rz = repeats_for_one_coordinates([m.cords[2] for m in moons], [m.cords[2] for m in moons])
+print("repeats for x, y, z: %d %d %d"%(rx,ry,rz))
+
+lcm = np.lcm.reduce([rx, ry, rz])
+print("Universe is repeated itself after %d steps"%lcm)
+    
+'''
+counter = 0
+while True:
+    counter += 1
     processed_moons = 1
     for m1 in moons[:-1]:
         for m2 in moons[processed_moons:]:
@@ -46,6 +93,10 @@ for t in range(0, 1000):
         processed_moons += 1 
     for m in moons:
         m.apply_velocity()
-
+    if all([m.is_in_original_state() for m in moons]):
+        print("Universe is repeated itself after %d steps"%counter)
+    print(str(counter))
+'''
 total_energy = sum([m.get_kinetic_energy() * m.get_potential_energy() for m in moons])
 print("Total energy in the Jupiter moons system is %d"%total_energy)
+
