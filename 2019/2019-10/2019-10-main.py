fe8cc273d69912d81cf342c310198feec55b417a
Michal Dmytruszynski <micdmy2@gmail.com>
2020-01-05 11:19:26 +0100

    2019-10-part-1 solved.

--- 2019/2019-10/2019-10-main.py
+++ 2019/2019-10/2019-10-main.py
@@ -1,3 +1,4 @@
+import math
 
 def read_input():
     with open("input.txt") as f:
@@ -9,8 +10,13 @@ class Asteroid():
     def __init__(self, x, y):
         self.x = x
         self.y = y
+        self.visible = {}
 
-    def angle_to(asteroid):
+    def angle_to(self, asteroid):
+        return math.atan2(asteroid.x - self.x, asteroid.y - self.y)
+
+    def add_visible(self, asteroid, angle):
+        self.visible[angle] = asteroid
 
 
 asteroids = []
@@ -19,3 +25,20 @@ for y, line in enumerate(lines):
     for x, char in enumerate(line):
         if char == '#':
             asteroids.append(Asteroid(x, y))
+
+for counter, asteroid in enumerate(asteroids):
+    print("Counter: %d"%counter)
+    for other_asteroid in asteroids:
+        if other_asteroid is not asteroid:
+            angle = asteroid.angle_to(other_asteroid)
+            asteroid.add_visible(other_asteroid, angle)
+
+max_visible_cnt = 0
+best_asteroid = None
+for asteroid in asteroids:
+    if len(asteroid.visible) > max_visible_cnt:
+        max_visible_cnt = len(asteroid.visible)
+        best_asteroid = asteroid
+
+print("Best asteroid for station is: x = %i, y = %i, cnt = %i"%(best_asteroid.x, best_asteroid.y, len(best_asteroid.visible)))
+
