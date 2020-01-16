22d9adf676a788fdb3eb458cb8071277119bcd5a
Michal Dmytruszynski <micdmy2@gmail.com>
2020-01-16 19:52:33 +0100

    2019-10-part-2 solved.

--- 2019/2019-10/2019-10-main.py
+++ 2019/2019-10/2019-10-main.py
@@ -1,4 +1,5 @@
 import math
+from collections import defaultdict
 
 def read_input():
     with open("input.txt") as f:
@@ -10,13 +11,42 @@ class Asteroid():
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.visible = {}
+        self.visible = defaultdict(list)
+        self.visible_sorted = []
+        self.destruction_idx = None
+
+    def __lt__(self, other):
+        return self.distance < other.distance
+
+    def __str__(self):
+        return "X: %i, Y: %i"%(self.x, self.y)
 
     def angle_to(self, asteroid):
         return math.atan2(asteroid.x - self.x, asteroid.y - self.y)
 
+    def count_distance(self, asteroid):
+        xd = asteroid.x - self.x
+        yd = asteroid.y - self.y
+        self.distance = math.sqrt(math.pow(yd,2) + math.pow(xd,2))
+
     def add_visible(self, asteroid, angle):
-        self.visible[angle] = asteroid
+        self.visible[angle].append(asteroid)
+        
+    def draw_map(self):
+        Xlen = 36
+        Ylen = 36
+        rows = [[" "]*Xlen for i in range(0,Ylen)]
+        rows[self.y][self.x] = '@'
+        for angle in self.visible:
+            for asteroid in self.visible[angle]:
+                if asteroid.destruction_idx:
+                    rows[asteroid.y][asteroid.x] = str(asteroid.destruction_idx)
+                else:
+                    rows[asteroid.y][asteroid.x] = '#'
+        print("")
+        for row in rows:
+            print("".join(row))
+
 
 
 asteroids = []
@@ -26,8 +56,8 @@ for y, line in enumerate(lines):
         if char == '#':
             asteroids.append(Asteroid(x, y))
 
+
 for counter, asteroid in enumerate(asteroids):
-    print("Counter: %d"%counter)
     for other_asteroid in asteroids:
         if other_asteroid is not asteroid:
             angle = asteroid.angle_to(other_asteroid)
@@ -35,10 +65,50 @@ for counter, asteroid in enumerate(asteroids):
 
 max_visible_cnt = 0
 best_asteroid = None
-for asteroid in asteroids:
+for cnt, asteroid in enumerate(asteroids):
     if len(asteroid.visible) > max_visible_cnt:
         max_visible_cnt = len(asteroid.visible)
         best_asteroid = asteroid
+        print("best %d"%cnt)
 
 print("Best asteroid for station is: x = %i, y = %i, cnt = %i"%(best_asteroid.x, best_asteroid.y, len(best_asteroid.visible)))
 
+
+station =  best_asteroid
+#station = asteroids[350] # from 1 part solution
+for other_asteroid in asteroids:
+    if other_asteroid is not station:
+        other_asteroid.count_distance(station)
+        angle = station.angle_to(other_asteroid)
+        station.add_visible(other_asteroid, angle)
+
+for angle in station.visible:
+    station.visible_sorted.append(angle)
+
+
+
+station.visible_sorted.sort()
+station.visible_sorted.reverse()
+
+for angle in station.visible:
+    asteroids_list = station.visible[angle]
+    asteroids_list.sort()
+
+cnt = 0    
+while cnt < 200:
+    for angle in station.visible_sorted:
+        asteroids_list = station.visible[angle]
+        for asteroid in asteroids_list:
+            if asteroid.destruction_idx == None:
+                cnt += 1
+                asteroid.destruction_idx = cnt
+                station.draw_map()
+                if cnt == 200:
+                    print("200th asteroid:")
+                    print(str(asteroid))
+                    exit()
+                break
+
+
+
+print("fdsfds")
