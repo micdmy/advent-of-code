387db7db5b0f97bdab65186040a45fd31b0188b6
Michal Dmytruszynski <micdmy2@gmail.com>
2021-12-04 23:23:57 +0100

    2021-03-part-02 solved.

--- 2021/2021-03/2021-03-main.py
+++ 2021/2021-03/2021-03-main.py
@@ -9,23 +9,51 @@ def parse_bits(string_with_bits):
 lines = read_input()
 L = len(lines[1])
 N = len(lines)
-bit_counter = [0 for x in range(0, L)]
 
-for line in read_input():
-    bits = parse_bits(line)
-    bit_counter = [bit_counter[i] + bits[i] for i in range(0, L)]
+def count_mcb(lines):
+    L = len(lines[1])
+    N = len(lines)
+    bit_counter = [0 for x in range(0, L)]
 
-MCB = []
-for cnt in bit_counter:
-    if cnt > N / 2:
-        MCB.append("1")
-    elif cnt < N / 2:
-        MCB.append("0")
-    else:
-        logging.error("The same number of 0 and 1 bits")
+    for line in lines:
+        bits = parse_bits(line)
+        bit_counter = [bit_counter[i] + bits[i] for i in range(0, L)]
+
+    MCB = []
+    for cnt in bit_counter:
+        if cnt >= N / 2:
+            MCB.append("1")
+        elif cnt < N / 2:
+            MCB.append("0")
+    return MCB
+
+MCB = count_mcb(lines)
 
 most_common_bits = int("".join(MCB), base=2)
-least_common_bits = (pow(2,12)-1) ^ most_common_bits
+least_common_bits = (pow(2, L)-1) ^ most_common_bits
+LCB = ["1" if b == "0" else "0" for b in MCB]
 print("MCB = {0:2}".format(most_common_bits))
 print("LCB = {0:2}".format(least_common_bits))
 print("Answer part 1 = {}".format(most_common_bits * least_common_bits))
+
+
+lines_copy = lines
+for i in range(0, L):
+    lines = [line for line in lines if line[i] == MCB[i]]    
+    if len(lines) == 1:
+        break
+    MCB = count_mcb(lines)
+
+ogr = int(lines[0], base=2)
+
+lines = lines_copy
+for i in range(0, L):
+    lines = [line for line in lines if line[i] == LCB[i]]    
+    if len(lines) == 1:
+        break
+    mcb = count_mcb(lines)
+    LCB = ["1" if b == "0" else "0" for b in mcb]
+
+csr = int(lines[0], base=2)
+
+print("ogr= {} csr= {} , multiplied = {}".format(ogr, csr, csr * ogr))
